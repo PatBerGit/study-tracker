76046e2f78dfd7709f99f528931c4eca4a14a352
Patrik Bergsängel <patrik.bergsangel@capgemini.com>
2023-08-16 19:44:57 +0200

    Added fields to settings component

---
+++ src/components/menu/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import styles from './style.module.scss';
+import Link from 'next/link';
+
+export default function Menu(){
+
+return(
+    <>
+    <menu>
+        <ul>
+            <li>configuration</li>
+        </ul>
+    </menu>
+    </>
+);
+
+
+}
