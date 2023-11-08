9bd9ef5c777bfbfa5760fc6d414a1679c9eefa3d
Patrik Bergsängel <patrik.bergsangel@capgemini.com>
2023-11-08 15:03:20 +0100

    more styling adjustments

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -11,7 +11,7 @@ return(
             <li className={styles.menuItem}>Courses</li>
             <li className={styles.menuItem}>Schools</li>
             <li className={styles.menuItem}>Calendar</li>
-            <Link href={"/signup"}><li className={styles.menuItem}>Signup</li></Link>
+            <Link className={styles.link} href={"/signup"}><li className={styles.menuItem}>Signup</li></Link>
         </ul>
     </menu>
     </>
